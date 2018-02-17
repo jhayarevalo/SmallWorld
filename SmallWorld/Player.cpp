87009dba81e55b2cfa371a33ed8abdf15b84d553
jhayarevalo <jhayarevalo@gmail.com>
2018-02-16 23:56:03 -0500

    #4 Player Class

--- SmallWorld/Player.cpp
+++ SmallWorld/Player.cpp
@@ -1,12 +1,62 @@
 #include "stdafx.h"
 #include "Player.h"
 
+#include <iostream>
 
-Player::Player()
+using namespace std;
+
+Player::Player(int pID)
 {
+	ID = pID;
+	coins1 = 5;
+	countVictoryCoins();
+}
+
+void Player::countVictoryCoins() {
+	victoryCoins = coins1 + coins3 * 3 + coins5 * 5 + coins10 * 10;
+}
+
+void Player::giveCoins(int type, int amount) {
+	if (type == 1) {
+		coins1 = amount;
+	}
+	else if (type == 3) {
+		coins3 = amount;
+	}
+	else if (type == 5) {
+		coins5 = amount;
+	}
+	else if (type == 10) {
+		coins10 = amount;
+	}
+	else {
+		cerr << "Invalid coin type." << endl;
+	}
+}
+
+void Player::picks_race(pair<Race, Badge> racePick) {
+	activeRace = racePick.first; //set active race
+	activeBadge = racePick.second; //set active badge
 }
 
 
-Player::~Player()
-{
+void Player::conquers(int rID) {
+	cout << "Player " << this->ID << " has conquered a region." << endl;
+}
+
+void Player::scores() {
+	cout << "\nPlayer " << this->ID << " has.." << endl;
+	cout << victoryCoins << " Victory Coins." << endl;
+	cout << ownedRegionIDs.size() << " Owned Regions." << endl;
+	if (!activeRace.isNull()) {
+		cout << "Active Race: " << activeRace.name << endl;
+		cout << "Active Badge: " << activeBadge.name << endl;
+	}
+	if (!declinedRace.isNull()) {
+		cout << "Declined Race: " << declinedBadge.name << endl;
+		cout << "Declined Race: " << declinedBadge.name << endl;
+	}
+
+	diceFacility.displayRollStats();
+
 }
--- SmallWorld/Player.h
+++ SmallWorld/Player.h
@@ -1,8 +1,47 @@
 #pragma once
+
+#include "Region.h"
+#include "Dice.h"
+#include "Race.h"
+#include "Badge.h"
+
 class Player
 {
 public:
-	Player();
-	~Player();
+	int ID;
+
+	//Victory Coins
+	int coins1;
+	int coins3;
+	int coins5;
+	int coins10;
+	int victoryCoins;
+
+	//Game Tokens
+	int activeRaceTokens;
+	int declinedRaceTokens;
+
+	vector<int> ownedRegionIDs;
+	Dice diceFacility;
+
+	Race activeRace;
+	Race declinedRace;
+
+	Badge activeBadge;
+	Badge declinedBadge;
+
+	Player(int pID);
+
+	void countVictoryCoins();
+	void giveCoins(int type, int amount);
+	void picks_race(pair<Race,Badge> racePick);
+	void displayRace();
+	void conquers(int rID);
+	void scores();
+
+	bool operator<(const Player &other) const {
+		return victoryCoins < other.victoryCoins;
+	}
+	
 };
 
